ef59eb7208a15c6e5de5abc30266384c79f05c39
LazBean <75616163+LazBean@users.noreply.github.com>
2024-02-06 05:02:12 +0900

    Update audio.js

--- js/audio.js
+++ js/audio.js
@@ -1,15 +1,30 @@
-
-
 (function() {
     var volume = 0.1;
+    var isUserInteracted = false;
+
+    document.addEventListener('click', function() {
+        isUserInteracted = true;
+    });
 
     window.audio = {
         play: function(clip, v = 1) {
             var a = new Audio(clip);
-			a.volume = volume * v;
-			a.play();
+            a.volume = volume * v;
+            
+            var playPromise = a.play();
+
+            if (playPromise !== undefined) {
+                playPromise.then(_ => {
+                    
+                }).catch(error => {
+                    if (!isUserInteracted) {
+                        console.log("Audio Playback is blocked until user interaction");
+                        
+                    } else {
+                        console.error("Audio play error:", error);
+                    }
+                });
+            }
         }
     };
 })();
-
-
